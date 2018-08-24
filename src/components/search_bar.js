1b8207e86e9a36307343f58d6c4aad055098e2e6
TessACraig89 <tesscraig.webdev@gmail.com>
2018-08-24 13:23:18 -0500

    defines render method on SearchBar class

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+
 // define SearchBar class and give it all the functionality that React.Component has
 class SeachBar extends React.Componenet{
-
+  render() {
+    
+  }
 }
 
 export default SearchBar;
