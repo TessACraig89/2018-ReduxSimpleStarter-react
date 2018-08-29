40f40379733463fbd80118ce2b19d30a574211c5
TessACraig89 <tesscraig.webdev@gmail.com>
2018-08-29 12:44:04 -0500

    console logs input value using eventhandler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,14 +3,15 @@ import React, { Component } from 'react';
 
 
 // define SearchBar class and give it all the functionality that React Component has
-// input elements have change events when a user types into them, to use the event you write "on""typeofevent"={this.nameofevent}
+// input elements have change events when a user types into them, to use the event you write "on""typeofevent"={this.nameofeventhandler}
+// create a new input elemment, pass it a prop onChange with a value of this.onInputChange
 class SearchBar extends Component {
   render() {
-    return <input onChange={this.onInputChange}/>;
+    return <input onChange={this.onInputChange} />;
   }
 
-  onInputChange() {
-
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
