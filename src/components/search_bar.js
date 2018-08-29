7840a60cb4f2c0aca4f3f9b518fc2e1b8dd72072
TessACraig89 <tesscraig.webdev@gmail.com>
2018-08-29 12:55:45 -0500

    condenses onChange handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,15 +3,10 @@ import React, { Component } from 'react';
 
 
 // define SearchBar class and give it all the functionality that React Component has
-// input elements have change events when a user types into them, to use the event you write "on""typeofevent"={this.nameofeventhandler}
 // create a new input elemment, pass it a prop onChange with a value of this.onInputChange
 class SearchBar extends Component {
   render() {
-    return <input onChange={this.onInputChange} />;
-  }
-
-  onInputChange(event) {
-    console.log(event.target.value);
+    return <input onChange={(event) => console.log(event.target.value)} />;
   }
 }
 
