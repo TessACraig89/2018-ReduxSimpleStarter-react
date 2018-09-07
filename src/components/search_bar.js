023711c6930e40094f95c61adae64d4780e1f5d5
TessACraig89 <tesscraig.webdev@gmail.com>
2018-09-07 12:58:00 -0500

    initializes SearchBar state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,14 @@ import React, { Component } from 'react';
 
 // define SearchBar class and give it all the functionality that React Component has
 // create a new input elemment, pass it a prop onChange with a value of this.onInputChange
+// input handler is passed to onChange, it automatically runs when the input is changed.
+// state is a plain
 class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { term: ''};
+  }
   render() {
     return <input onChange={(event) => console.log(event.target.value)} />;
   }
