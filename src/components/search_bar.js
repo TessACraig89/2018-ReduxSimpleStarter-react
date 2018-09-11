3480097ea09f29e620a255e5fe5f20a6c3b42832
TessACraig89 <tesscraig.webdev@gmail.com>
2018-09-11 17:52:17 -0500

    changes searchBar input field into a controlled field using value = {this.state.term}

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,18 +5,18 @@ import React, { Component } from 'react';
 // define SearchBar class and give it all the functionality that React Component has
 // create a new input elemment, pass it a prop onChange with a value of this.onInputChange
 // input handler is passed to onChange, it automatically runs when the input is changed.
-// state is a plain
 class SearchBar extends Component {
   constructor(props){
     super(props);
 
-    this.state = { term: ''};
+    this.state = { term: 'starting'};
   }
   render() {
     return (
       <div>
-        <input onChange={event => this.setState({ term: event.target.value})} />
-        Value of the input: {this.state.term}
+        <input
+          value = {this.state.term}
+          onChange={event => this.setState({ term: event.target.value})} />
       </div>
     );
   }
