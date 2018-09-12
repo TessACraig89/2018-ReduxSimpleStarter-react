cf6903c1c7ea31b2664da469321b8320a812e7bf
TessACraig89 <tesscraig.webdev@gmail.com>
2018-09-12 18:47:54 -0500

    declares imageURL variable of video snippet, adds as src for img, and adds video.snippet.title to media-heading

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,15 +1,15 @@
  import React from 'react';
 
  const VideoListItem = ({video}) => {
+   const imageURL = video.snippet.thumbnails.default.url;
     return (
       <li className="list-group-item">
         <div className="video-list media">
           <div className="mdedia-left">
-            <img className="media-object" />
+            <img className="media-object" src={imageURL}/>
           </div>
-
           <div className="media-body">
-            <div className="media-heading"></div>
+            <div className="media-heading">{video.snippet.title}</div>
           </div>
         </div>
       </li>
