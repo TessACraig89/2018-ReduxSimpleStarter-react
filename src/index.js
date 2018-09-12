e7b31856246190def42cb92c758e22e55d304d16
TessACraig89 <tesscraig.webdev@gmail.com>
2018-09-11 19:08:39 -0500

    accesses YTSearch by importing,calls YTSearch function, passing 2 arguments, an object with key of API_KEY and term of what we are searching, and a function that logs data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBcSh-Bl_3gphRm5mbkYM7gXTr081UK8k4';
+
+YTSearch({key: API_KEY, term: 'cats'}, function(data) {
+    console.log(data);
+});
 // Create a new componenet. This component should produce
 // some html
 const App = () => {
