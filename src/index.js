a82e207b4a09148ea8dd0d2ff998dbf0e1a2150f
TessACraig89 <tesscraig.webdev@gmail.com>
2018-09-11 19:19:39 -0500

    changes App component from functional to class based component by adding { Component } to react import and updating to *class App extends Component*

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBcSh-Bl_3gphRm5mbkYM7gXTr081UK8k4';
 
 YTSearch({key: API_KEY, term: 'cats'}, function(data) {
     console.log(data);
 });
-// Create a new componenet. This component should produce
-// some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
-// Take this componenets generatied HTML and put it
-// on the page (in the DOM)
+
 ReactDOM.render(<App />, document.querySelector('.container'));
