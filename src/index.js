e25184f8b1c8f63f16a2e8f0829f7fc537ec57ee
TessACraig89 <tesscraig.webdev@gmail.com>
2018-09-11 21:07:44 -0500

    sets up VideoList functional component, adds VideoList into App render method, passing list of videos from App to VideoList as prop using *videos={this.state.videos}*

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBcSh-Bl_3gphRm5mbkYM7gXTr081UK8k4';
 
 
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
